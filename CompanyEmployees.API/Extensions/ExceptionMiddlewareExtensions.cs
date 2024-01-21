31fc03c25dc18082d029dbfe0f0fbd53e99eebc8
pauloosilas <paulo3356@gmail.com>
2024-01-21 20:18:09 -0300

    create company collection

--- CompanyEmployees.API/Extensions/ExceptionMiddlewareExtensions.cs
+++ CompanyEmployees.API/Extensions/ExceptionMiddlewareExtensions.cs
@@ -21,6 +21,7 @@ namespace CompanyEmployees.API.Extensions
                         context.Response.StatusCode = contextFeature.Error switch
                         {
                             NotFoundException => StatusCodes.Status404NotFound,
+                            BadRequestException => StatusCodes.Status400BadRequest,
                             _ => StatusCodes.Status500InternalServerError
                         };
                         logger.LogError($"Something went wrong: {contextFeature.Error}");
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -21,5 +21,9 @@ namespace Repository
             FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 
         public void CreateCompany(Company company) => Create(company);
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => 
+            FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
     }
+
 }
